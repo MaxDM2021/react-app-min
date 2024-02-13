20af3c6e2a370c2bf346c6ba45a781dfbb659b7f
Maksym Dmitriiev <maximdm22@gmail.com>
2024-02-13 12:02:14 +0000

    fin

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,21 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
+import useInput from "./hooks/useInput";
 
 export default function EffectSection() {
-const [modal, setModal] = useState(false);
+  const input = useInput();
+  const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  
 
+  const fetchUsers = useCallback(async () => {
+    setLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    setUsers(users);
+    setLoading(false);
+  }, []);
 
-
-
-  function openModal() {
-    setModal(true)
-  }
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
 
   return (
     <section>
       <h3>Effects</h3>
-      <Button onClick={openModal}>Открыть информацию</Button>
+      <Button style={{ marginBottom: "1rem" }} onClick={() => setModal(true)}>
+        Открыть информацию
+      </Button>
 
       <Modal open={modal}>
         <h3>Hello from modal</h3>
@@ -25,9 +37,21 @@ const [modal, setModal] = useState(false);
           quia illum tempore! Aliquid iusto eveniet porro eaque perferendis,
           libero magni? Alias?
         </p>
-        <Button onClick={() => setModal(false)} >Close modal</Button>
+        <Button onClick={() => setModal(false)}>Close modal</Button>
       </Modal>
-    
+      {loading && <p>Loading...</p>}
+
+      {!loading && (
+        <>
+        <input type="text" className="control" {... input} />
+   
+          <ul>
+            {users.filter((user) => user.name.toLowerCase().includes(input.value.toLowerCase())).map((user) => (
+              <li key={user.id}>{user.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </section>
   );
 }
